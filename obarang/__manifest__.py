387cda092b789bc09d3b60a4c9dba64166ce4e4a
Ilham arsy <Ilhamarsy16@gmail.com>
2024-04-28 13:09:37 +0800

    init commit

---
+++ obarang/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Pembelian Barang',
+    'summary': 'Ini adalah applikasi pembelian barang',
+    'version': '1.0',
+    'category': 'custom',
+    'author': 'Ilham',
+    'maintainer': 'Ilham',
+    'website':  'obarang.co.id',
+    'license': 'AGPL-3',
+    'data': [
+        'security/ir.model.access.csv',
+    ],
+}
