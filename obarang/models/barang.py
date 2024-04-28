e0bd8bba767704f894aa62b3e904e779120dfac2
Ilham arsy <Ilhamarsy16@gmail.com>
2024-04-28 13:10:15 +0800

    buat model barang

---
+++ obarang/models/barang.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from odoo import api, fields, models, _
+
+class Produk(models.Model):
+    _name = 'obarang.barang' 
+    _description = 'Ini adalah master produk'
+
+
+    name = fields.Char('Nama Barang', required=True)
+
+    kode_barang = fields.Char(string="Kode Barang")
+
+    stok = fields.Integer(required=True)
+
+    harga = fields.Float(required=True)
+
+    berat = fields.Float(default=0)
+
+    state = fields.Selection(string='Status', selection=[
+        ('available', 'Tersedia'),
+        ('unavailable', 'Tidak Tersedia'),
+    ])
